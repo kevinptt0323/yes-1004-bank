576bc559dcdb1ac410fe94804c3d1474dd1543a4
Kevin Lin <kevinptt0323@gmail.com>
2015-04-13 22:00:21 +0800

    add class Message for api returning value

--- src/api/include/include.php
+++ src/api/include/include.php
@@ -1,6 +1,25 @@
 <?php
 require_once("auth.php");
 function getPDO() {
-	return new PDO("mysql:host=127.0.0.1;charset=UTF8;dbname=".DB, DBUSER, DBPASSWD, array(PDO::ATTR_PERSISTENT=>true));
+	try {
+		$db = new PDO("mysql:host=127.0.0.1;charset=UTF8;dbname=".DB, DBUSER, DBPASSWD, array(PDO::ATTR_PERSISTENT=>true));
+		$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+		return $db;
+	} catch (PDOException $e) {
+		echo 'Connection failed: ' . $e->getMessage();
+	}
 }
+
+class Message {
+	public $state, $success, $message;
+	public static $SUCCESS = "success";
+	public static $ERROR = "error";
+	public function __construct($_state, $_message) {
+		$this->state = $_state;
+		$this->success = $_state==self::$SUCCESS;
+		$this->message = $_message;
+		return $this;
+	}
+};
+
 ?>
