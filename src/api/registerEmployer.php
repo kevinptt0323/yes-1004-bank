42352bb1de775d958815ed1b03e2832915a6060f
Kevin Lin <kevinptt0323@gmail.com>
2015-04-14 21:54:00 +0800

    avoid same username for employers and job-seekers

--- src/api/registerEmployer.php
+++ src/api/registerEmployer.php
@@ -7,14 +7,16 @@ function signupEmployer($data) {
 		return new Message(Message::$ERROR, "Cannot have empty field.");
 	}
 	$db = getPDO();
-	$query_user = $db->prepare("select count(*) from `employer` where `account` = :username");
+	$query_jobseeker = $db->prepare("select count(*) from `user` where `account` = :username");
+	$query_employer = $db->prepare("select count(*) from `employer` where `account` = :username");
 	try {
-		$query_user->execute(array(':username' => $data['username']));
+		$query_jobseeker->execute(array(':username' => $data['username']));
+		$query_employer->execute(array(':username' => $data['username']));
 	} catch (PDOException $e) {
 		return new Message(Message::$ERROR, $e->getMessage() . "<br />Please contact administrator.");
 	}
 
-	if( $query_user->fetch()[0] == 1 ) {
+	if( $query_jobseekeer->fetch()[0] || $query_employer->fetch()[0] ) {
 		return new Message(Message::$ERROR, "Username \"$data[username]\" existed.");
 	} else {
 		$insert = $db->prepare("insert into `employer` (`account`, `password`, `phone`, `email`) values (:username, sha2(:password, 256), :phone, :email)");
--- src/api/registerJobseeker.php
+++ src/api/registerJobseeker.php
@@ -7,14 +7,16 @@ function signupJobseeker($data) {
 		return new Message(Message::$ERROR, "Cannot have empty field.");
 	}
 	$db = getPDO();
-	$query_user = $db->prepare("select count(*) from `user` where `account` = :username");
+	$query_jobseeker = $db->prepare("select count(*) from `user` where `account` = :username");
+	$query_employer = $db->prepare("select count(*) from `employer` where `account` = :username");
 	try {
-		$query_user->execute(array(':username' => $data['username']));
+		$query_jobseeker->execute(array(':username' => $data['username']));
+		$query_employer->execute(array(':username' => $data['username']));
 	} catch (PDOException $e) {
 		return new Message(Message::$ERROR, $e->getMessage() . "<br />Please contact administrator.");
 	}
 
-	if( $query_user->fetch()[0] == 1 ) {
+	if( $query_jobseekeer->fetch()[0] || $query_employer->fetch()[0] ) {
 		return new Message(Message::$ERROR, "User Existed");
 	} else {
 		$insert = $db->prepare("insert into `user` (`account`, `password`, `phone`, `gender`, `age`, `email`, `expected_salary`, `education`) values (:username, sha2(:password, 256), :phone, :gender, :age, :email, :salary, :education)");
