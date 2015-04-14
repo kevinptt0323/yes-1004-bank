3272ca8d94a3b41210bb27351e59faf3d2bccdbe
Kevin Lin <kevinptt0323@gmail.com>
2015-04-14 15:10:43 +0800

    add form validation in register employer; rewrite viewer functions

--- src/js/view.js
+++ src/js/view.js
@@ -15,11 +15,14 @@ view.factory('pageView', function() {
 });
 
 view.service('view', ['$http', function($http) {
-  this.registerJobseekerView = {
-    init: function() {
-      jQuery('.ui.dropdown').dropdown();
-    },
-    process: function(formData) {
+
+  this.registerJobseekerView = function() {
+    var that = {};
+    var $form = jQuery('#register-jobseeker-form')
+    that.init = function() {
+      $form.find('.ui.dropdown').dropdown();
+    };
+    that.process = function(formData) {
       console.log("process form!");
       $http({
         method  : 'POST',
@@ -38,32 +41,82 @@ view.service('view', ['$http', function($http) {
           console.log("Error");
         })
       ;
-    }
-  };
-  this.registerEmployerView = {
-    init: function() {
-      jQuery('.ui.dropdown').dropdown();
-    },
-    process: function(formData) {
+    };
+    return that;
+  }();
+
+  this.registerEmployerView = function() {
+    var that = {};
+    var $form = jQuery('#register-employer-form')
+    that.init = function() {
+      $form.form({
+        username: {
+          identifier : 'username',
+          rules: [{
+            type : 'empty',
+            prompt : 'Please enter a username'
+          }]
+        },
+        password: {
+          identifier : 'password',
+          rules: [{
+            type : 'empty',
+            prompt : 'Please enter a password'
+          }]
+        },
+        phone: {
+          identifier : 'phone',
+          rules: [{
+            type : 'empty',
+            prompt : 'Please enter your phone'
+          }]
+        },
+        email: {
+          identifier : 'email',
+          rules: [{
+            type : 'empty',
+            prompt : 'Please enter your e-mail'
+          },{
+            type : 'email',
+            prompt : 'Please enter a valid e-mail address'
+          }]
+        }
+      });
+    };
+    that.validate = function(success_cb, error_cb) {
+      console.log('validate');
+      
+      var result = $form.form('validate form');
+      if( result && angular.isFunction(success_cb) ) {
+        success_cb();
+      }
+      if( !result && angular.isFunction(error_cb) ) {
+        error_cb();
+      }
+    };
+    that.process = function(formData) {
       console.log("process form!");
-      $http({
-        method  : 'POST',
-        url     : 'api/registerEmployer.php',
-        data    : $.param(formData),
-        headers : { 'Content-Type': 'application/x-www-form-urlencoded' }
-      })
-        .success(function(data) {
-          if( data.success ) {
-            console.log("success!");
-          } else {
-            console.log("Error: " + data.message);
-          }
+      this.validate(function() {
+        $http({
+          method  : 'POST',
+          url     : 'api/registerEmployer.php',
+          data    : $.param(formData),
+          headers : { 'Content-Type': 'application/x-www-form-urlencoded' }
         })
-        .error(function() {
-          console.log("Error");
-        })
-      ;
-    }
-  };
+          .success(function(data) {
+            if( data.success ) {
+              console.log("success!");
+            } else {
+              console.log("Error: " + data.message);
+            }
+          })
+          .error(function() {
+            console.log("Error");
+          })
+        ;
+      });
+    };
+    return that;
+  }();
 }]);
 
