db81688346f5c4f8b0d6346b45366aff0fd09fad
Kevin Lin <kevinptt0323@gmail.com>
2015-05-04 16:00:39 +0800

    sort in jobs list

--- src/api/jobsList.php
+++ src/api/jobsList.php
@@ -5,7 +5,16 @@ session_start();
 function jobsList() {
 	$db = getPDO();
 	try {
-		$list = $db->query("select * from `recruit` order by `id`")->fetchAll(PDO::FETCH_ASSOC);
+		if( isset($_GET['row']) && isset($_GET['order']) ) {
+			if( $_GET['order'] == "ascending" ) {
+				$order = "asc";
+			} else if( $_GET['order'] == "descending" ) {
+				$order = "desc";
+			}
+			$list = $db->query("select * from `recruit` order by $_GET[row] $order")->fetchAll(PDO::FETCH_ASSOC);
+		} else {
+			$list = $db->query("select * from `recruit` order by `id`")->fetchAll(PDO::FETCH_ASSOC);
+		}
 		return new Message(Message::$SUCCESS, $list);
 	} catch (PDOException $e) {
 		return new Message(Message::$ERROR, $e->getMessage() . "<br />Please contact administrator.");
