e67005b7c692ad1305db997e3feff98ee06b41a3
Kevin Lin <kevinptt0323@gmail.com>
2015-05-10 22:28:14 +0800

    use prepare statement to query database

--- src/api/apply.php
+++ src/api/apply.php
@@ -9,12 +9,12 @@ function newApply($data) {
 	if( empty($data['rid']) ) {
 		return new Message(Message::$ERROR, "Cannot have empty field.");
 	}
-	escape($data);
 	$db = getPDO();
-	$insert = $db->prepare("insert into `application` (user_id, recruit_id) values (?, ?)");
+	$insert = $db->prepare("insert into application (user_id, recruit_id) values (?, ?)");
+	$find_stat = $db->prepare("select count(*) from application where user_id=? and recruit_id=?");
 	try {
-		$find = $db->query("select count(*) from `application` where user_id='" . $_SESSION['user']['id'] . "' and recruit_id='" . $data['rid'] . "'");
-		if( $find->fetch()[0] != 0 ) {
+		$find_stat->execute(array($_SESSION['user']['id'], $data['rid']));
+		if( $find_stat->fetch()[0] != 0 ) {
 			return new Message(Message::$ERROR, "You've applied this recruit, please be patient.");
 		} else {
 			$insert->execute(array($_SESSION['user']['id'], $data['rid']));
