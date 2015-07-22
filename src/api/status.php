82ff907f6f5dc84612e223f0c50190be10215a94
Kevin Lin <kevinptt0323@gmail.com>
2015-07-22 17:22:43 +0800

    add more information in status api

--- src/api/status.php
+++ src/api/status.php
@@ -8,6 +8,17 @@ function status() {
 	);
 	if( $status["isLogin"] ) {
 		$status["user"] = $_SESSION['user'];
+		$db = getPDO();
+		switch ($status["user"]["type"]) {
+			case "employer":
+				$query = $db->prepare("select phone, email from `employer` where `account` = :username");
+				break;
+			case "jobseeker":
+				$query = $db->prepare("select phone, email from `user` where `account` = :username");
+				break;
+		}
+		$query->execute(array(':username' => $status["user"]["name"]));
+		$status["user"] = array_merge($status["user"], $query->fetch(PDO::FETCH_ASSOC));
 	}
 	return new Message(Message::$SUCCESS, $status);
 }
